c2030ee9b035e53ea3faa37e1ca36e0f8cd24afb
Aditi Gupta <161322460+withaditi25@users.noreply.github.com>
2024-08-28 10:52:23 +0530

    Update Nvbar.js

---
+++ myapp/src/Nvbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+function Nvbar(){
+  return(
+    <>
+    <p>HOME </p>
+     <p>CONTACT  </p>
+      <p>ABOUT</p>
+     <p>BLOG </p>
+    </>
+    
