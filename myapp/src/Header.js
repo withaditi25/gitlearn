e0f2e500b854a6bcba955f2066d397e3f812face
Aditi Gupta <161322460+withaditi25@users.noreply.github.com>
2024-08-29 09:44:20 +0530

    Create Header.js

---
+++ myapp/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header(){
+  return(
+    <>
+    <h1>This is Git learning website </h1>
+     <p> In the tutorial we learn about Git and Git commands </p>
+    </>
+  );
+}
+export default Header ;
