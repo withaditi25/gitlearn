78ac8dc7b0a43f4c9de9b896917e6df3a1ba55a0
Aditi <aditi2501gupta@gmail.com>
2024-08-29 09:50:50 +0530

    updates

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Nvbar from './Nvbar';
+import Header  from './Header';
 
 function App() {
   return (
     <>
     <h1>hello</h1>
+    <Nvbar/>
+    <Header/>
     </>
   );
 }
